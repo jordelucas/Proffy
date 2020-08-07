2d07323c87361558a7cb42bdf830567112c1dc52
Jordevá Lucas <jordevalucas@hotmail.com>
2020-08-07 00:16:51 -0300

    cria aula

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -31,13 +31,13 @@ function TeacherList () {
             name="week_day" 
             label="Dia da semana"
             options={[
-              { value: '6', label: 'Domingo-feita' },
-              { value: '0', label: 'Segunda-feita' },
-              { value: '1', label: 'Terça-feita' },
-              { value: '2', label: 'Quarta-feita' },
-              { value: '3', label: 'Quinta-feita' },
-              { value: '4', label: 'Sexta-feita' },
-              { value: '5', label: 'Sábado-feita' }
+              { value: '0', label: 'Domingo' },
+              { value: '1', label: 'Segunda-feita' },
+              { value: '2', label: 'Terça-feita' },
+              { value: '3', label: 'Quarta-feita' },
+              { value: '4', label: 'Quinta-feita' },
+              { value: '5', label: 'Sexta-feita' },
+              { value: '6', label: 'Sábado' }
             ]}
           />
           <Input type="time" name="time" label="Hora"/>
