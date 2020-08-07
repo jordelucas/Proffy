6a804c01e73b08fe9e3d74497adc89aae12f545f
Jordevá Lucas <jordevalucas@hotmail.com>
2020-08-07 18:59:36 -0300

    add assets

---
+++ app/src/pages/Landing/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View } from 'react-native';
+
+import styles from './styles'
+
+const Landing: React.FC = () => {
+  return (
+    <View />
+  )
+}
+
+export default Landing;
