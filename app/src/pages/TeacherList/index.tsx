d137a34b9df64a672c34f7947520f450671c16c6
Jordevá Lucas <jordevalucas@hotmail.com>
2020-08-08 00:55:52 -0300

    implementa lógica de favoritar e persistir escolha

--- app/src/pages/TeacherList/index.tsx
+++ app/src/pages/TeacherList/index.tsx
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, ScrollView, TextInput } from 'react-native';
 import {  BorderlessButton, RectButton } from 'react-native-gesture-handler';
+import AsyncStorage from '@react-native-community/async-storage'
 
 import { Feather } from '@expo/vector-icons'
 
@@ -13,18 +14,32 @@ import styles from './styles'
 
 const TeacherList: React.FC = () => {
   const [ isFiltersVisible, setIsFiltersVisible ] = useState(false)
-  
   const [ teachers, setTeachers ] = useState([])
-
+  const [ favorites, setFavorites ] = useState<number[]>([])
   const [ subject, setSubject] = useState('')
   const [ week_day, setWeekDay] = useState('')
   const [ time, setTime] = useState('')
+
+  function loadFavorites() {
+    AsyncStorage.getItem('favorites').then(response => {
+      if (response) {
+        const favoritedTeachers = JSON.parse(response);
+        const favoritedTeachersIds = favoritedTeachers.map((teacher: Teacher) => {
+          return teacher.id;
+        })
+
+        setFavorites(favoritedTeachersIds);
+      }
+    })
+  }
 
   function handleToggleFiltersVisible() {
     setIsFiltersVisible(!isFiltersVisible)
   }
 
   async function handleFiltersSubmit() {
+    loadFavorites();
+
     const response = await api.get('classes', {
       params: {
         subject,
@@ -98,7 +113,13 @@ const TeacherList: React.FC = () => {
         }}
       >
         {teachers.map((teacher: Teacher) => {
-          return <TeacherItem key={teacher.id} teacher={teacher}/>
+          return (
+            <TeacherItem 
+              key={teacher.id} 
+              teacher={teacher}
+              favorited={favorites.includes(teacher.id)}
+            />
+          )
         })}
       </ScrollView>
     </View>
